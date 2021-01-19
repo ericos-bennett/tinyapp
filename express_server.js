b4b8ffd95da9fe5133fbea3dc245aa5558cc0224
Eric <ericos.bennett@gmail.com>
2021-01-19 01:09:41 +0000

    added /urls get handler, blank urls_index.ejs template file

--- express_server.js
+++ express_server.js
@@ -13,12 +13,13 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
